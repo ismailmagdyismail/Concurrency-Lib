ef8df42a454a137e56a1cb5e74175daae7158a2c
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail00882@yahoo.com>
2025-05-17 21:36:12 +0300

    [Userware]: add userware example for usage of cahnnels

---
+++ Userwrare/UnBufferedChannel/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <thread>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <atomic>
+
+#include "UnBufferedChannel.h"
+
+class CounterActor
+{
+public:
+    void Increment()
+    {
+        int delta = 1;
+        m_oUpdateChannel.SendValue(delta);
+    }
+
+    void Decrement()
+    {
+        int delta = -1;
+        m_oUpdateChannel.SendValue(delta);
+    }
+
+    void Stop()
+    {
+        m_bStopSignal = true;
+        m_oUpdateChannel.Close();
+    }
+
+    void Run()
+    {
+        static std::ofstream logFile{"./counterActor.log.txt"};
+        while (!m_bStopSignal)
+        {
+            //! Read and update value
+            int delta;
+            bool bReadValue = m_oUpdateChannel.ReadValue(delta);
+            if (!bReadValue)
+            {
+                logFile << "Error in reading from channel" << std::endl;
+                return;
+            }
+            m_iValue += delta;
+            logFile << "Change: " << delta << " , newValue: " << m_iValue << std::endl;
+        }
+    }
+
+private:
+    UnBufferedChannel<int> m_oUpdateChannel;
+    int m_iValue{0};
+    std::atomic<bool> m_bStopSignal{false};
+};
+
+void incrementThread(CounterActor &p_oCounter)
+{
+    // static std::ofstream logFile{"./increment.log.txt"};
+    // std::random_device rd;
+    // std::mt19937 gen(rd());
+    // std::uniform_int_distribution<> dist(10, 100); // sleep time in ms
+
+    for (int i = 0; i < 100; i++)
+    {
+        p_oCounter.Increment();
+        // logFile << val << std::endl;
+        // std::this_thread::sleep_for(std::chrono::milliseconds(dist(gen)));
+    }
+}
+
+void decrementThread(CounterActor &p_oCounter)
+{
+    // static std::ofstream logFile{"./decrement.log.txt"};
+    // std::random_device rd;
+    // std::mt19937 gen(rd());
+    // std::uniform_int_distribution<> dist(10, 100); // sleep time in ms
+
+    for (int i = 0; i < 50; i++)
+    {
+        p_oCounter.Decrement();
+        // logFile << val << std::endl;
+        // std::this_thread::sleep_for(std::chrono::milliseconds(dist(gen)));
+    }
+}
+
+int main()
+{
+    CounterActor actor;
+    std::thread t([&actor]()
+                  { incrementThread(actor); });
+    std::thread t2([&actor]()
+                   { decrementThread(actor); });
+    std::thread t3(std::bind(&CounterActor::Run, std::ref(actor)));
+    // t3.detach();
+
+    t.join();
+    t2.join();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    actor.Stop();
+    t3.join();
+}
